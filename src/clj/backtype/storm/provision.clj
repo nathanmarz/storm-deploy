624c1ac18e580c1566ed9f1b65f5c960262593f0
Antoni Batchelli <tbatchelli@acm.org>
2014-04-04 14:30:11 -0700

    Clean-up code in provision.clj.

--- src/clj/backtype/storm/provision.clj
+++ src/clj/backtype/storm/provision.clj
@@ -6,8 +6,7 @@
         [pallet.core]
         [com.palletops.jclouds.compute2 :only [nodes-in-group]]
         [backtype.storm.util :only [with-var-roots]]
-        [clojure.tools.logging]
-        )
+        [clojure.tools.logging])
   (:require [pallet.configure])
   (:require [pallet.compute.jclouds])
   (:require [backtype.storm.node :as node])
@@ -18,28 +17,33 @@
 
 ;; memoize this
 (defn my-region []
-  (-> (pallet.configure/pallet-config) :services :default :jclouds.regions)
-  )
+  (-> (pallet.configure/pallet-config) :services :default :jclouds.regions))
 
 (defn jclouds-group [& group-pieces]
   (str "jclouds#" (apply str group-pieces)))
 
 (defn- print-ips-for-tag! [aws tag-str]
-  (let [running-node (filter running? (map (partial pallet.compute.jclouds/jclouds-node->node aws) (nodes-in-group aws tag-str)))]
-    (info (str "TAG:     " tag-str))
-    (info (str "PUBLIC:  " (clojure.string/join ", " (map primary-ip running-node))))
-    (info (str "PRIVATE: " (clojure.string/join ", " (map private-ip running-node))))))
+  (let [running-node
+        (filter running?
+                (map
+                 (partial pallet.compute.jclouds/jclouds-node->node aws)
+                 (nodes-in-group aws tag-str)))]
+    (infof "TAG:     %s" tag-str)
+    (infof "PUBLIC:  %s"
+           (clojure.string/join ", " (map primary-ip running-node)))
+    (infof "PRIVATE: %s"
+           (clojure.string/join ", " (map private-ip running-node)))))
 
 (defn print-all-ips! [aws name]
-  (let [all-tags [(str "zookeeper-" name) (str "nimbus-" name) (str "supervisor-" name)]]
+  (let [all-tags
+        [(str "zookeeper-" name) (str "nimbus-" name) (str "supervisor-" name)]]
        (doseq [tag all-tags]
          (print-ips-for-tag! aws tag))))
 
 (defn converge! [name branch commit aws sn zn nn]
   (converge {(node/nimbus name branch commit) nn
              (node/supervisor name branch commit) sn
-             (node/zookeeper name) zn
-             }
+             (node/zookeeper name) zn}
             :compute aws))
 
 (defn sync-storm-conf-dir [aws name]
@@ -51,31 +55,31 @@
     (spit (str conf-dir "/supervisor.yaml") supervisor-yaml)))
 
 (defn attach! [aws name]
-  (info "Attaching to Available Cluster...")
-  (sync-storm-conf-dir aws name)
-  (authorizeme aws (jclouds-group "nimbus-" name) 80 (my-region))
-  (authorizeme aws (jclouds-group "nimbus-" name) (node/storm-conf "nimbus.thrift.port") (my-region))
-  (authorizeme aws (jclouds-group "nimbus-" name) (node/storm-conf "ui.port") (my-region))
-  (authorizeme aws (jclouds-group "nimbus-" name) (node/storm-conf "drpc.port") (my-region))
-  (info "Attaching Complete."))
+  (let [nimbus-group (jclouds-group "nimbus-" name)
+        region (my-region)]
+    (info "Attaching to Available Cluster...")
+    (sync-storm-conf-dir aws name)
+    (authorizeme aws nimbus-group (jclouds-group "nimbus-" name) 80 region)
+    (authorizeme aws nimbus-group (node/storm-conf "nimbus.thrift.port") region)
+    (authorizeme aws nimbus-group (node/storm-conf "ui.port") region)
+    (authorizeme aws nimbus-group (node/storm-conf "drpc.port") region)
+    (info "Attaching Complete.")))
 
 (defn start-with-nodes! [aws name nimbus supervisor zookeeper]
   (let [nimbus (node/nimbus* name nimbus)
         supervisor (node/supervisor* name supervisor)
         zookeeper (node/zookeeper name zookeeper)
         sn (int (node/clusters-conf "supervisor.count" 1))
-        zn (int (node/clusters-conf "zookeeper.count" 1))]
-    (info (format "Provisioning nodes [nn=1, sn=%d, zn=%d]" sn zn))
-    (converge {nimbus 1
-              supervisor sn
-              zookeeper zn
-              }
-            :compute aws
-            )
+        zn (int (node/clusters-conf "zookeeper.count" 1))
+        region (my-region)
+        nimbus-group (jclouds-group "numbus-" name)
+        supervisor-group (jclouds-group "supervisor-" name)]
+    (infof "Provisioning nodes [nn=1, sn=%d, zn=%d]" sn zn)
+    (converge {nimbus 1 supervisor sn zookeeper zn} :compute aws)
     (debug "Finished converge")
 
-    (authorize-group aws (my-region) (jclouds-group "nimbus-" name) (jclouds-group "supervisor-" name))
-    (authorize-group aws (my-region) (jclouds-group "supervisor-" name) (jclouds-group "nimbus-" name))
+    (authorize-group aws region nimbus-group supervisor-group)
+    (authorize-group aws region supervisor-group nimbus-group)
     (debug "Finished authorizing groups")
 
     (lift nimbus :compute aws :phase [:post-configure :exec])
@@ -87,27 +91,30 @@
     (print-all-ips! aws name)))
 
 (defn start! [aws name branch commit]
-  (println "Starting cluster with storm branch " branch (if (empty? commit) "" (str " and commit " commit)))
-  (start-with-nodes! aws name (node/nimbus-server-spec name branch commit) (node/supervisor-server-spec name branch commit) (node/zookeeper-server-spec))
-  )
+  (println "Starting cluster with storm branch "
+           branch
+           (if (empty? commit) "" (str " and commit " commit)))
+  (start-with-nodes! aws name
+                     (node/nimbus-server-spec name branch commit)
+                     (node/supervisor-server-spec name branch commit)
+                     (node/zookeeper-server-spec)))
 
 (defn upgrade-with-nodes! [aws name nimbus supervisor zookeeper]
   (let [nimbus (node/nimbus* name nimbus)
         supervisor (node/supervisor* name supervisor)
         zookeeper (node/zookeeper name zookeeper)]
-;    (authorize-group aws (my-region) (jclouds-group "nimbus-" name) (jclouds-group "supervisor-" name))
-;    (authorize-group aws (my-region) (jclouds-group "supervisor-" name) (jclouds-group "nimbus-" name))
-
-;    (lift zookeeper :compute aws :phase [:configure])
-;    (lift nimbus :compute aws :phase [:configure :post-configure :exec])
     (lift supervisor :compute aws :phase [:configure :post-configure :exec])
     (println "Upgrade Complete.")))
 
 
 (defn upgrade! [aws name branch commit]
-  (println "Upgrading cluster with storm branch " branch (if (empty? commit) "" (str " and commit " commit)))
-  (upgrade-with-nodes! aws name (node/nimbus-server-spec name branch commit) (node/supervisor-server-spec name branch commit) (node/zookeeper-server-spec))
-  )
+  (println "Upgrading cluster with storm branch "
+           branch
+           (if (empty? commit) "" (str " and commit " commit)))
+  (upgrade-with-nodes! aws name
+                       (node/nimbus-server-spec name branch commit)
+                       (node/supervisor-server-spec name branch commit)
+                       (node/zookeeper-server-spec)))
 
 (defn stop! [aws name]
   (println "Shutting Down nodes...")
@@ -124,8 +131,7 @@
         user (-> (storm/storm-config "default")
                  :environment
                  :user
-                 (util/resolve-keypaths))
-        ]
+                 (util/resolve-keypaths))]
     (System/setProperty "jna.nosys" "true")
     (with-var-roots [node/*USER* user]
       (with-command-line args
