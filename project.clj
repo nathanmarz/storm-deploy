fd3bd2eb9d70674fa9ad74ac479739035883209c
Antoni Batchelli <tbatchelli@acm.org>
2014-04-04 12:42:33 -0700

    Update ssh-agent libraries.

--- project.clj
+++ project.clj
@@ -10,7 +10,10 @@
 
   :dependencies [[storm "0.5.4"]
                  [commons-codec "1.4"]
-                 [org.cloudhoist/pallet "0.7.5"]
+                 [org.cloudhoist/pallet "0.7.5"
+                  :exclusions
+                  [jsch-agent-proxy/jsch-agent-proxy-jna
+                   jsch-agent-proxy]]
                  [com.palletops/pallet-jclouds "1.7.0-alpha.3"]
                  [org.cloudhoist/java "0.5.0"]
                  [org.cloudhoist/git "0.5.0"]
@@ -25,6 +28,14 @@
                  [org.apache.jclouds.driver/jclouds-sshj "1.7.1"]
                  [org.apache.jclouds.provider/aws-ec2 "1.7.1"]
                  [org.apache.jclouds.provider/aws-s3 "1.7.1"]
+
+                 [com.jcraft/jsch "0.1.51"]
+                 [com.jcraft/jsch.agentproxy.usocket-jna "0.0.7"]
+                 [com.jcraft/jsch.agentproxy.usocket-nc "0.0.7"]
+                 [com.jcraft/jsch.agentproxy.sshagent "0.0.7"]
+                 [com.jcraft/jsch.agentproxy.pageant "0.0.7"]
+                 [com.jcraft/jsch.agentproxy.core "0.0.7"]
+                 [com.jcraft/jsch.agentproxy.jsch "0.0.7"]
 
                  [log4j/log4j "1.2.14"]
                  [jvyaml "1.0.0"]]
